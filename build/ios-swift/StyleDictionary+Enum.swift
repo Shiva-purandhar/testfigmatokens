76982af8e32079cb5af16a5d1fe14a8a5bf7c800
kavyakoka <kavya.koka@innovapptive.com>
2023-08-23 15:34:03 +0530

    pushing new chnagees

---
+++ build/ios-swift/StyleDictionary+Enum.swift
@@ -0,0 +1,31 @@
+
+//
+// StyleDictionary+Enum.swift
+//
+
+// Do not edit directly
+// Generated on Wed, 23 Aug 2023 10:01:55 GMT
+
+
+import UIKit
+
+public enum StyleDictionaryEnum {
+    public static let baseColorsBlack = #212121
+    public static let baseColorsWhite = #ffffff
+    public static let blue50 = rgba(61,90,255,0.5)
+    public static let blue8 = rgba(61,90,255,0.08)
+    public static let blueDark = #2A3eb1
+    public static let blueLight = #637bfe
+    public static let blueMain = #3d5afe
+    public static let bodyTextBold = [object Object]
+    public static let bodyTextMedium = [object Object]
+    public static let bodyTextRegular = [object Object]
+    public static let border1 = 1px
+    public static let padding12 = 12px
+    public static let padding24 = 24px
+    public static let padding6 = 6px
+    public static let padding8 = 8px
+    public static let radius4 = 4px
+    public static let radius7 = 7px
+    public static let roboto = Roboto
+}
---
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -0,0 +1,31 @@
+
+//
+// StyleDictionary+Struct.swift
+//
+
+// Do not edit directly
+// Generated on Wed, 23 Aug 2023 10:01:55 GMT
+
+
+import UIKit
+
+internal struct StyleDictionaryStruct {
+    internal static let baseColorsBlack = #212121
+    internal static let baseColorsWhite = #ffffff
+    internal static let blue50 = rgba(61,90,255,0.5)
+    internal static let blue8 = rgba(61,90,255,0.08)
+    internal static let blueDark = #2A3eb1
+    internal static let blueLight = #637bfe
+    internal static let blueMain = #3d5afe
+    internal static let bodyTextBold = [object Object]
+    internal static let bodyTextMedium = [object Object]
+    internal static let bodyTextRegular = [object Object]
+    internal static let border1 = 1px
+    internal static let padding12 = 12px
+    internal static let padding24 = 24px
+    internal static let padding6 = 6px
+    internal static let padding8 = 8px
+    internal static let radius4 = 4px
+    internal static let radius7 = 7px
+    internal static let roboto = Roboto
+}
